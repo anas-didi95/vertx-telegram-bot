33dc266fe3adec2d7e017a80a979937136de181a
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-05 21:22:47 +0800

    Extract project

---
+++ src/main/java/com/anas/bot/MainVerticle.java
@@ -0,0 +1,23 @@
+package com.anas.bot;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(5000, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 5000");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
