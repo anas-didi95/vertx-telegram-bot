2a85c9c1243739207dd19535c2bf0079a4935acb
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-06 16:54:37 +0800

    Add implementation

--- src/main/java/com/anasdidi/bot/MainVerticle.java
+++ src/main/java/com/anasdidi/bot/MainVerticle.java
@@ -1,6 +1,7 @@
 package com.anasdidi.bot;
 
 import com.anasdidi.bot.common.AppConfig;
+import com.anasdidi.bot.domain.greeting.GreetingVerticle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,32 +9,72 @@ import org.apache.logging.log4j.Logger;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   private static final Logger logger = LogManager.getLogger(MainVerticle.class);
+  private EventBus eventBus;
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    final String TAG = "[start]";
     ConfigRetriever configRetriever = ConfigRetriever.create(vertx,
         new ConfigRetrieverOptions().addStore(new ConfigStoreOptions().setType("env")));
 
     configRetriever.rxGetConfig().subscribe(config -> {
       AppConfig appConfig = AppConfig.create(config);
-      logger.info("appConfig\n{}", appConfig.toString());
+      logger.info("{} appConfig\n{}", TAG, appConfig.toString());
 
-      vertx.createHttpServer().requestHandler(req -> {
-        req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
-      }).listen(appConfig.getAppPort(), appConfig.getAppHost(), http -> {
-        if (http.succeeded()) {
-          logger.info("HTTP server started on {}:{}", appConfig.getAppHost(), appConfig.getAppPort());
-          startPromise.complete();
-        } else {
-          startPromise.fail(http.cause());
-        }
-      });
+      Router router = Router.router(vertx);
+      router.route().handler(BodyHandler.create());
+      router.post("/").handler(this::requestHandler);
+
+      this.eventBus = vertx.eventBus();
+      vertx.deployVerticle(new GreetingVerticle(eventBus));
+
+      Router contextPath = Router.router(vertx).mountSubRouter("/bot", router);
+      int port = appConfig.getAppPort();
+      String host = appConfig.getAppHost();
+      vertx.createHttpServer().requestHandler(contextPath).rxListen(port, host).subscribe(server -> {
+        logger.info("HTTP server started on {}:{}", appConfig.getAppHost(), appConfig.getAppPort());
+        startPromise.complete();
+      }, e -> startPromise.fail(e));
+      /*
+       * vertx.createHttpServer().requestHandler(req -> {
+       * req.response().putHeader("content-type",
+       * "text/plain").end("Hello from Vert.x!"); }).listen(appConfig.getAppPort(),
+       * appConfig.getAppHost(), http -> { if (http.succeeded()) {
+       * logger.info("HTTP server started on {}:{}", appConfig.getAppHost(),
+       * appConfig.getAppPort()); startPromise.complete(); } else {
+       * startPromise.fail(http.cause()); } });
+       */
     }, e -> startPromise.fail(e));
+  }
+
+  private void requestHandler(RoutingContext routingContext) {
+    final String TAG = "[requestHandler]";
+    JsonObject requestBody = routingContext.getBodyAsJson();
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("{} requestBody\n{}", TAG, requestBody.encodePrettily());
+    }
+
+    String event = requestBody.getString("event");
+    eventBus.rxRequest(event, requestBody.encode()).subscribe(handler -> {
+      JsonObject response = new JsonObject((String) handler.body());
+      routingContext.response().setStatusCode(200).putHeader("content-type", "application/json").end(response.encode());
+    }, e -> {
+      JsonObject response = new JsonObject()
+          .put("status", new JsonObject().put("isSuccess", false).put("message", "Get greeting failed!"))
+          .put("error", e.getMessage());
+      routingContext.response().setStatusCode(400).putHeader("content-type", "application/json").end(response.encode());
+    });
   }
 }
---
+++ src/main/java/com/anasdidi/bot/domain/greeting/GreetingVerticle.java
@@ -0,0 +1,38 @@
+package com.anasdidi.bot.domain.greeting;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.EventBus;
+
+public class GreetingVerticle extends AbstractVerticle {
+
+  private static final Logger logger = LogManager.getLogger(GreetingVerticle.class);
+  private final EventBus eventBus;
+
+  public GreetingVerticle(EventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    eventBus.consumer("get-greeting", handler -> {
+      JsonObject request = new JsonObject((String) handler.body());
+      JsonObject response = new JsonObject()
+          .put("status", new JsonObject().put("isSuccess", true).put("message", "Greeting received."))
+          .put("data", new JsonObject().put("value", "Hello"));
+      String message = response.encode();
+
+      if (logger.isDebugEnabled()) {
+        logger.debug("[start] event={}, request\n{}", "get-greeting", request.encodePrettily());
+        logger.debug("[start] event={}, response\n{}", "get-greeting", response.encodePrettily());
+      }
+
+      logger.info("[start] event={}, message={}", "get-greeting", message);
+      handler.reply(message);
+    });
+  }
+}
