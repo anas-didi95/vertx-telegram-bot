baa878f727436ca3cbaa759ce933aa1d78fa5df1
Anas Juwaidi <anas.didi95@gmail.com>
2020-11-10 20:16:09 +0800

    Add test to check response message

---
+++ src/test/java/com/anasdidi/bot/api/expense/TestExpenseVerticle.java
@@ -0,0 +1,47 @@
+package com.anasdidi.bot.api.expense;
+
+import com.anasdidi.bot.MainVerticle;
+import com.anasdidi.bot.common.AppConstants;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.core.Vertx;
+
+@ExtendWith(VertxExtension.class)
+public class TestExpenseVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void testSaveStatusSuccess(Vertx vertx, VertxTestContext testContext) {
+    JsonObject requestBody = new JsonObject()//
+        .put("message", new JsonObject()//
+            .put("text", "/expense")//
+            .put("from", new JsonObject()//
+                .put("id", 000)));
+
+    vertx.eventBus().rxRequest("/expense", requestBody).subscribe(response -> {
+      testContext.verify(() -> {
+        JsonObject responseBody = (JsonObject) response.body();
+        Assertions.assertNotNull(responseBody);
+
+        String expectedMessage = new StringBuilder()//
+            .append("Budget saved")//
+            .toString();
+        Assertions.assertEquals(expectedMessage, responseBody.getString("message"));
+
+        testContext.completeNow();
+      });
+    }, e -> testContext.failNow(e));
+
+  }
+}
